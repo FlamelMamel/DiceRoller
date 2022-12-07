f36e4a02b4d03eed654ceb354e341339706a35cb
Flamel <flamelmamel@gmail.com>
2022-12-07 17:08:46 +0600

    gg

---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.assignment1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.roll)
+        button.setOnClickListener {
+            RollDice()
+        }
+    }
+
+    private fun RollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+    class Dice(val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+}
